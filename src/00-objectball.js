18173726ea3a00b6eeff468e71385487a5cb282c
cpan1995 <cpan1995@gmail.com>
2021-06-29 17:38:11 -0400

    commit tree d1290ea5e4a133ef91b1f69c96501656b09f05f6

---
+++ src/00-objectball.js
@@ -0,0 +1,62 @@
+function gameObject(){
+    const home = {
+        teamName: "Brooklyn Nets",
+        colors: "blue",
+        players: [{
+            playerName: "Alan Anderson",
+            number: 0,
+            shoe: 16,
+            points: 22,
+            rebounds: 12,
+            assists: 12,
+            steals: 3,
+            blocks: 1,
+            slamDunks: 1
+        },
+        {
+            playerName: "Reggie Evans",
+            number: 30,
+            shoe: 14,
+            points: 12,
+            rebounds: 12,
+            assists: 12,
+            steals: 12,
+            blocks: 12,
+            slamDunks: 7
+        },
+        {
+            playerName: "Brook Lopez",
+            number: 11,
+            shoe: 17,
+            points: 17,
+            rebounds: 19,
+            assists: 10,
+            steals: 3,
+            blocks: 1,
+            slamDunks: 15
+        },
+        {
+            playerName: "Mason Plumlee",
+            number: 1,
+            shoe: 19,
+            points: 26,
+            rebounds: 12,
+            assists: 6,
+            steals: 3,
+            blocks: 8,
+            slamDunks: 5  
+        },
+        {
+            playerName: "Jason Terry",
+            number: 31,
+            shoe: 15,
+            points: 19,
+            rebounds: 2,
+            assists: 2,
+            steals: 4,
+            blocks: 11,
+            slamDunks: 1
+        }
+    ]
+    }
+}
